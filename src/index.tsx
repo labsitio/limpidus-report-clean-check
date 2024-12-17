6046a5c9da4047d2997fbe2956de2b79f5a015e1
Gerson Ribeiro <gerson.ribeiro@terceirizados.celcoin.com.br>
2024-12-17 09:14:00 -0300

    fixing routes

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './i18n';
+import { BrowserRouter } from 'react-router-dom';
 // import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
